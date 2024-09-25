eb4f477336e278088afc91940a763c63731c8218
Nicki Hartmann <nicki.j.hartmann@gmail.com>
2024-09-25 21:54:05 +0700

    ⚡️ update user validation

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.enableCors();
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Strip unrecognized properties
+      forbidNonWhitelisted: true, // Throw an error if unrecognized properties are included
+      transform: true, // Automatically transform payloads to DTO instances
+    }),
+  );
 
   const config = new DocumentBuilder()
     .setTitle('Carbon Coins API')
--- src/module/user/user.dto.ts
+++ src/module/user/user.dto.ts
@@ -1,6 +1,6 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { UserRole } from '@prisma/client';
-import { IsEnum, IsString } from 'class-validator';
+import { IsEnum, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 import { BaseGetAllDto, BaseGetOneDto } from 'src/base/get.dto';
 
 export class GetOneUserDto extends BaseGetOneDto {}
@@ -10,19 +10,23 @@ export class GetAllUserDto extends BaseGetAllDto {}
 export class CreateUserDto {
   @ApiProperty({ required: true })
   @IsString()
+  @IsNotEmpty()
   username: string;
 
   @ApiProperty({ required: true })
   @IsString()
+  @IsNotEmpty()
   password: string;
 
   @ApiProperty({ required: true })
   @IsString()
+  @IsNotEmpty()
   email: string;
 
   @ApiProperty({ required: true })
   @IsString()
-  wallet_address: string;
+  @IsOptional()
+  wallet_address?: string;
 
   @ApiProperty()
   @IsEnum(UserRole)
